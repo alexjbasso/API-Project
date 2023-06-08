ec009cc491ab75c3d2e076110fb7748a3f1dbd5e
Alex <alex.j.basso@gmail.com>
2023-06-08 17:35:38 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,16 +1,20 @@
 const router = require('express').Router();
-const { restoreUser } = require('../../utils/auth.js');
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-
-
-//===Middleware===//
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
-  router.use(restoreUser);
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+router.use(restoreUser);
 
+// Connecting routers
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 
 //===Route Handlers===//
---
+++ backend/routes/api/session.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+
+// Login attempt
+router.post(
+  '/',
+  async (req, res, next) => {
+    // Get submitted username/email and password from request body
+    const { credential, password } = req.body;
+
+    //Find user in the database that matches credentials
+    const user = await User.unscoped().findOne({
+      where: {
+        // either username or email
+        [Op.or]: {
+          username: credential,
+          email: credential
+        }
+      }
+    });
+
+    // If the password submitted doesn't matched the stored hased password, throw a 401 error
+    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = { credential: 'The provided credentials were invalid.' };
+      return next(err);
+    }
+
+    // If the password does match, then create a user object of the user logging in
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+
+    // Set token cookie with authorized user object
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+      user: safeUser
+    });
+  }
+)
+
+module.exports = router
